740fe24eb390510ea681732fa67e923844e3dc84
Kyrylo Mahda <kyrylomahda@gmail.com>
2022-01-28 21:57:01 +0200

    add new solution for task11

--- task_11_sort_the_odd.py
+++ task_11_sort_the_odd.py
@@ -1,11 +1,17 @@
 #https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/python
 #You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
+def isEven(n):
+    return n % 2 == 0
+
 def sort_array(source_array):
+    evens_counter = 0
+    sorted_odds = sorted([x for x in source_array if not isEven(x)])
     for i in range(len((source_array))):
-        if (source_array[i] % 2) != 0:
-            for n in range(len((source_array[i+1:]))):
-                if ((source_array[n+i+1] % 2) != 0) and source_array[i] > source_array[n+i+1]:
-                    source_array[i],source_array[n+i+1] = source_array[n+i+1],source_array[i]        
+        if isEven(source_array[i]):
+            evens_counter += 1
+            continue
+        source_array[i] = sorted_odds[i - evens_counter ]       
     return source_array
+
 
