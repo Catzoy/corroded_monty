be62a2edbfde6a5868db52b297871c03d714192e
Kyrylo Mahda <kyrylomahda@gmail.com>
2022-02-04 10:57:41 +0200

    removes extra files from task11_to_rust

--- rust_versions/target/rls/debug/deps/rust_versions-bd9695e8d9fd605d.d
+++
@@ -1,5 +0,0 @@
-d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-bd9695e8d9fd605d.rmeta: src\main.rs
-
-d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-bd9695e8d9fd605d.d: src\main.rs
-
-src\main.rs:
--- rust_versions/target/rls/debug/deps/rust_versions-cce64ad4aed431e0.d
+++
@@ -1,5 +0,0 @@
-d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-cce64ad4aed431e0.rmeta: src\main.rs
-
-d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-cce64ad4aed431e0.d: src\main.rs
-
-src\main.rs:
