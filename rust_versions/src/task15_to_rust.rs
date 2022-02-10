d350f4fee64d985043800f4b172a44cf44235e52
Kyrylo Mahda <kyrylomahda@gmail.com>
2022-02-10 23:16:44 +0200

    add updated solution for task15

--- rust_versions/src/task15_to_rust.rs
+++ rust_versions/src/task15_to_rust.rs
@@ -13,16 +13,16 @@ pub fn next_bigger_number(n: i64) -> i64 {
     let digits = create_sorted_vec(n);
     let mut digits1 = create_sorted_vec(n).clone();
     digits1.reverse();
-    let mut max_num:i64 = 0;
-    for elem in digits1 {
+    let mut max_num = 0 as i128;
+    for elem  in digits1 {
         max_num *= 10;
-        max_num += elem;
+        max_num += elem as i128;
     }
-    let mut output = n;
-    while output <= max_num{
+    let mut output = n as i128;
+    while output <= max_num as i128{
           output += 1;
-          if create_sorted_vec(output) == digits{
-              return output;
+          if create_sorted_vec(output as i64) == digits{
+              return output as i64;
           }
       }
     return -1
