4f77158799644e66811238cb671b465f9f7a021e
Kyrylo Mahda <kyrylomahda@gmail.com>
2022-02-13 21:12:54 +0200

    add solution for task16

---
+++ rust_versions/target/debug/deps/rust_versions.d
@@ -0,0 +1,6 @@
+D:\corroded_monty\corroded_monty\rust_versions\target\debug\deps\rust_versions.exe: src\main.rs src\task15_to_rust.rs
+
+D:\corroded_monty\corroded_monty\rust_versions\target\debug\deps\rust_versions.d: src\main.rs src\task15_to_rust.rs
+
+src\main.rs:
+src\task15_to_rust.rs:
---
+++ rust_versions/target/rls/debug/deps/rust_versions-bd9695e8d9fd605d.d
@@ -0,0 +1,6 @@
+d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-bd9695e8d9fd605d.rmeta: src\main.rs src\task15_to_rust.rs
+
+d:\corroded_monty\corroded_monty\rust_versions\target\rls\debug\deps\rust_versions-bd9695e8d9fd605d.d: src\main.rs src\task15_to_rust.rs
+
+src\main.rs:
+src\task15_to_rust.rs:
